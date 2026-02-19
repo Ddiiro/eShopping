b14a1b3fbca555c4b54edd276b750ea2ee3bb102
Ddiiro Joseph <ddiiro92@gmail.com>
2026-02-19 23:29:09 +0300

    Add the track order component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage'
 import OrderPage from './pages/OrderPage';
 import CheckoutPages from './pages/CheckoutPages';
+import TrackingPage from './pages/TrackingPage';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/orders" element={<OrderPage />} />
         <Route path="/checkout" element={<CheckoutPages />} />
+        <Route path="/tracking" element={<TrackingPage />} />
     </Routes>
     </BrowserRouter>
   )
