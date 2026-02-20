c5a10eb1079e4a1076e364e347238f0f725c191c
Ddiiro <Ddiiro@ddiiro.local>
2026-02-20 10:35:26 +0300

    Move the brower route to main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage'
 import OrderPage from './pages/OrderPage';
 import CheckoutPages from './pages/CheckoutPages';
@@ -7,14 +7,12 @@ import TrackingPage from './pages/TrackingPage';
 function App() {
 
   return (
-    <BrowserRouter>
     <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/orders" element={<OrderPage />} />
         <Route path="/checkout" element={<CheckoutPages />} />
         <Route path="/tracking" element={<TrackingPage />} />
     </Routes>
-    </BrowserRouter>
   )
 }
 
