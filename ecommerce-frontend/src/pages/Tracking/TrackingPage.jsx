c22b9cd434e7bbbe69811b6aff50c1f65b1cda8f
Ddiiro Joseph <ddiiro92@gmail.com>
2026-03-05 22:09:10 +0300

    populate the tracking page data

--- ecommerce-frontend/src/pages/Tracking/TrackingPage.jsx
+++ ecommerce-frontend/src/pages/Tracking/TrackingPage.jsx
@@ -1,57 +1,64 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { Fragment } from 'react'
 import '../Tracking/tracking.css'
 import Header from '../../components/Header'
 import { useContext } from 'react';
 import { ordersContext } from '../../App';
 import { useParams } from 'react-router-dom';
+import dayjs  from 'dayjs';
 
 function TrackingPage() {
-    const {cart,orders} = useContext(ordersContext);
-    const {orderId, productId} = useParams();
-    
-  return (
-    <div>
-        <Header cartItems={cart} />
-        <div class="tracking-page">
-            <div class="order-tracking">
-                <a class="back-to-orders-link link-primary" href="orders.html">
-                View all orders
-                </a>
+    const { cart, orders } = useContext(ordersContext);
+    const { orderId, productId } = useParams();
 
-                <div class="delivery-date">
-                Arriving on Monday, June 13
-                </div>
+    const orderItem = orders.find((order) => order.id === orderId);
+    const product = orderItem?.products.find((product) => product.productId === productId);
 
-                <div class="product-info">
-                Black and Gray Athletic Cotton Socks - 6 Pairs
-                </div>
+    return (
+        <div>
+            <Header cartItems={cart} />
+            {product &&
+                < Fragment key={product.id}>
+                    <div class="tracking-page">
+                        <div class="order-tracking">
+                            <a class="back-to-orders-link link-primary" href="orders.html">
+                                View all orders
+                            </a>
 
-                <div class="product-info">
-                Quantity: {cart.quantity}
-                </div>
+                            <div class="delivery-date">
+                                Arriving on {dayjs(product.estimatedDeliveryTimeMs).format('dddd MMM DD')}
+                            </div>
 
-                <img class="product-image" src="public/assets/images/products/athletic-cotton-socks-6-pairs.jpg" />
+                            <div class="product-info">
+                                {product.product.name}
+                            </div>
 
-                <div class="progress-labels-container">
-                <div class="progress-label">
-                    Preparing
-                </div>
-                <div class="progress-label current-status">
-                    Shipped
-                </div>
-                <div class="progress-label">
-                    Delivered
-                </div>
-                </div>
+                            <div class="product-info">
+                                Quantity: {product.quantity}
+                            </div>
 
-                <div class="progress-bar-container">
-                <div class="progress-bar"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
+                            <img class="product-image" src={product.product.image} />
+
+                            <div class="progress-labels-container">
+                                <div class="progress-label">
+                                    Preparing
+                                </div>
+                                <div class="progress-label current-status">
+                                    Shipped
+                                </div>
+                                <div class="progress-label">
+                                    Delivered
+                                </div>
+                            </div>
+
+                            <div class="progress-bar-container">
+                                <div class="progress-bar"></div>
+                            </div>
+                        </div>
+                    </div>
+                </Fragment>
+            }
+        </div >
+    )
 }
 
 export default TrackingPage
