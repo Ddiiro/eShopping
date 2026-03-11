a10a658c737a3a4e2a694a00f75506696ec0ecfe
Ddiiro Joseph <ddiiro92@gmail.com>
2026-03-11 18:17:49 +0300

    Load the set up for the logic of selecting a delivery date

--- ecommerce-frontend/src/pages/Checkout/OrderSummary.jsx
+++ ecommerce-frontend/src/pages/Checkout/OrderSummary.jsx
@@ -27,7 +27,7 @@ function OrderSummary( {fetchPaymentSummary} ) {
     }, [])
 
     const getSelectedDeliveryOption = (deliveryOptionID) => {
-        console.log("Cart item delivery option ID:", deliveryOptionID);
+        console.log("Cart item delivery option ID:", deliveryOptionID); 
         setSelectedDeliveryOptionId(deliveryOptionID);
         console.log("Selected delivery option ID:", selectedDeliveryOptionId);
     }
