509559fe982c4bc56216e89dc69ee021cc49364f
Ddiiro Joseph <ddiiro92@gmail.com>
2026-03-12 23:55:13 +0300

    Add logic for updating delvery option chosen

--- ecommerce-frontend/src/pages/Checkout/OrderSummary.jsx
+++ ecommerce-frontend/src/pages/Checkout/OrderSummary.jsx
@@ -8,7 +8,7 @@ import CartItemDetails from './CartItemDetails';
 
 
 
-function OrderSummary( {fetchPaymentSummary} ) {
+function OrderSummary({ fetchPaymentSummary }) {
     const { cart } = useContext(ordersContext);
     const [deliveryOptions, setDeliveryOptions] = useState([])
     const [selectedDeliveryOptionId, setSelectedDeliveryOptionId] = useState(null);
@@ -28,63 +28,76 @@ function OrderSummary( {fetchPaymentSummary} ) {
 
     const getSelectedDeliveryOption = (deliveryOptionID) => {
         setSelectedDeliveryOptionId(deliveryOptionID);
-    }
+        if (cart.length > 0) {
+            const cartItem = cart[0];
+            axios.post('/api/cart/delivery-option', {
+                productId: cartItem.productId,
+                deliveryOptionId: deliveryOptionID
+            })
+                .then((response) => {
+                    fetchPaymentSummary();
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+            }
+        }
 
-    return (
-        <div>
-            <div class="order-summary">
-                {
-                    cart.map((cartItem) => {
-                        const selectedDeliveryOption = deliveryOptions.find((deliveryOption) => {
-                            return deliveryOption.id === cartItem.deliveryOptionId;
-                        })
-                        return (
-                            <div key={cartItem.productId} class="cart-item-container">
-                                <div class="delivery-date">
-                                    Delivery date: {selectedDeliveryOption ? dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd MMMM D') : "Not selected"}
-                                </div>
+        return (
+            <div>
+                <div class="order-summary">
+                    {
+                        cart.map((cartItem) => {
+                            const selectedDeliveryOption = deliveryOptions.find((deliveryOption) => {
+                                return deliveryOption.id === cartItem.deliveryOptionId;
+                            })
+                            return (
+                                <div key={cartItem.productId} class="cart-item-container">
+                                    <div class="delivery-date">
+                                        Delivery date: {selectedDeliveryOption ? dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd MMMM D') : "Not selected"}
+                                    </div>
 
-                                <div class="cart-item-details-grid">
-                                    <img class="product-image"
-                                        src={cartItem.product.image} />
+                                    <div class="cart-item-details-grid">
+                                        <img class="product-image"
+                                            src={cartItem.product.image} />
 
-                                    <CartItemDetails cartItem={cartItem} fetchPaymentSummary={fetchPaymentSummary} />
+                                        <CartItemDetails cartItem={cartItem} fetchPaymentSummary={fetchPaymentSummary} />
 
-                                    <div class="delivery-options">
-                                        <div class="delivery-options-title">
-                                            Choose a delivery option:
-                                        </div>
-                                        {
-                                            deliveryOptions.map((deliveryOption) => {
-                                                return (
-                                                    <div class="delivery-option" key={deliveryOption.id}>
-                                                        <input type="radio"
-                                                            class="delivery-option-input"
-                                                            name={`delivery-option-${cartItem.productId}`}
-                                                            checked={selectedDeliveryOptionId}
-                                                            onChange={() => getSelectedDeliveryOption(deliveryOption.id)}
-                                                        />
-                                                        <div>
-                                                            <div class="delivery-option-date">
-                                                                {dayjs(deliveryOption.estimatedDeliveryTimeMs).format('dddd MMMM D')}
-                                                            </div>
-                                                            <div class="delivery-option-price">
-                                                                {deliveryOption.priceCents === 0 ? 'FREE Shipping' : `${formatCurrency(deliveryOption.priceCents)} - Shipping`}
+                                        <div class="delivery-options">
+                                            <div class="delivery-options-title">
+                                                Choose a delivery option:
+                                            </div>
+                                            {
+                                                deliveryOptions.map((deliveryOption) => {
+                                                    return (
+                                                        <div class="delivery-option" key={deliveryOption.id}>
+                                                            <input type="radio"
+                                                                class="delivery-option-input"
+                                                                name={`delivery-option-${cartItem.productId}`}
+                                                                checked={selectedDeliveryOptionId}
+                                                                onChange={() => getSelectedDeliveryOption(deliveryOption.id)}
+                                                            />
+                                                            <div>
+                                                                <div class="delivery-option-date">
+                                                                    {dayjs(deliveryOption.estimatedDeliveryTimeMs).format('dddd MMMM D')}
+                                                                </div>
+                                                                <div class="delivery-option-price">
+                                                                    {deliveryOption.priceCents === 0 ? 'FREE Shipping' : `${formatCurrency(deliveryOption.priceCents)} - Shipping`}
+                                                                </div>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                )
-                                            })
-                                        }
+                                                    )
+                                                })
+                                            }
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                }
+                            )
+                        })
+                    }
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 
-export default OrderSummary
+    export default OrderSummary
