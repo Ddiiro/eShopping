db98ecd9f261c656eb9f18ec0607866e64b0d0c7
Ddiiro Joseph <ddiiro92@gmail.com>
2026-04-02 18:30:42 +0300

    add test for other possible current cout come formats

--- ecommerce-frontend/src/utils/currency.test.js
+++ ecommerce-frontend/src/utils/currency.test.js
@@ -4,3 +4,8 @@ import { formatCurrency } from './currency'
 it('converts 1091 cents as $10.91', () => {
     expect(formatCurrency(1091)).toBe('$10.91');
 });
+
+it('converts 100 to 1.0', () => {
+    expect(formatCurrency(100)).toBe('$1.00');
+    expect(formatCurrency(1090)).toBe('$10.90')
+})
